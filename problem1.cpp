ca4c6c1e8e084dab5d4adf01faf9b9f47e10eef3
kr-trilok <trilokkumar2002@gmail.com>
2024-07-16 18:36:05 +0530

    remove question discription fome Problem1.cpp file

--- problem1.cpp
+++ problem1.cpp
@@ -1,7 +1,3 @@
-/*Given an integer array and two integers L and R. Find the sum of elements between the
-index L and index R.
-
-Note: Both L and R inclusive.*/
 
 #include<iostream>
 #include<vector>
